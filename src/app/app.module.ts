8359a48bfe7bbc062baff7319cd31464814178f4
Ike Okafor <okaforim@miamioh.edu>
2023-04-25 22:24:42 -0400

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Host, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductEditComponent } from './products/product-edit/product-edit.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductCreateComponent } from './products/product-create/product-create.component';
+import { VendorCreateComponent } from './vendors/vendor-create/vendor-create.component';
+import { VendorDetailComponent } from './vendors/vendor-detail/vendor-detail.component';
+import { VendorEditComponent } from './vendors/vendor-edit/vendor-edit.component';
+import { VendorListComponent } from './vendors/vendor-list/vendor-list.component';
+import { UserCreateComponent } from './users/user-create/user-create.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserLoginComponent } from './users/user-login/user-login.component';
+import { RequestCreateComponent } from './requests/request-create/request-create.component';
+import { RequestDetailComponent } from './requests/request-detail/request-detail.component';
+import { RequestEditComponent } from './requests/request-edit/request-edit.component';
+import { RequestListComponent } from './requests/request-list/request-list.component';
+import { RequestReviewComponent } from './requests/request-review/request-review.component';
+import { ReviewDetailComponent } from './requests/review-detail/review-detail.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
@@ -13,6 +32,26 @@ import { ProductEditComponent } from './products/product-edit/product-edit.compo
     AppComponent,
     ProductListComponent,
     ProductEditComponent,
+    ProductDetailComponent,
+    ProductCreateComponent,
+    VendorCreateComponent,
+    VendorDetailComponent,
+    VendorEditComponent,
+    VendorListComponent,
+    UserCreateComponent,
+    UserDetailComponent,
+    UserEditComponent,
+    UserListComponent,
+    UserLoginComponent,
+    RequestCreateComponent,
+    RequestDetailComponent,
+    RequestEditComponent,
+    RequestListComponent,
+    RequestReviewComponent,
+    ReviewDetailComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
   
   ],
   imports: [
