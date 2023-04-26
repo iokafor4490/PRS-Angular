aff31371451aeb40c53521288989724ef0898713
Ike Okafor <okaforim@miamioh.edu>
2023-04-25 22:07:24 -0400

    User

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductEditComponent } from './products/product-edit/product-edit.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    ProductEditComponent,
   
   ],
   imports: [
