bec42bc40e86e83687fe6bb8b17222ba3acd3890
Ike Okafor <okaforim@miamioh.edu>
2023-04-29 17:00:23 -0400

    Changes to requests

--- src/app/requests/request-review/request-review.component.ts
+++ src/app/requests/request-review/request-review.component.ts
@@ -7,7 +7,34 @@ import { Component } from '@angular/core';
 })
 export class RequestReviewComponent {
 pageTitle: string = "Request Review";
-requests: any[] = [];
+requests: any[] = [
 
+
+  {
+    "id": 1,
+    "description": "test request",
+    "justification": "test",
+    "rejectionReason": "test",
+    "deliveryMode": "test",
+    "submittedDate": "2023-03-08T00:00:00",
+    "dateNeeded": "2023-01-18",
+    "status": "Approved",
+    "total": 3629.7,
+    "user": {
+        "id": 1,
+        "userName": "username",
+        "firstName": "firstName",
+        "lastName": "lastName",
+        "phone": "5135551234",
+        "email": "admin@email.com",
+        "isReviewer": true,
+        "isAdmin": true
+    }
+}
+
+];
 
 }
+
+
+
