cce55f004122d714c1bda45963695c1e6a33d102
Ike Okafor <okaforim@miamioh.edu>
2023-04-29 12:01:43 -0400

    Vendor/Product

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ProductDetailComponent {
   pageTitle: string = "Product Detail";
-  products: any[] = [];
+  products: any= {}
 
 }
--- src/app/vendors/vendor-detail/vendor-detail.component.ts
+++ src/app/vendors/vendor-detail/vendor-detail.component.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class VendorDetailComponent {
   pageTitle: string = "Vendor Detail";
-  
+  vendor: any= 
+  {
+    "id": 1,
+    "code": "CODE-1234",
+    "name": "Wally World",
+    "address": "123 Capitalism Lane",
+    "city": "Cincinnati",
+    "state": "OH",
+    "zip": "45202",
+    "phone": "5135551234",
+    "email": "customercare@wallyworld.net"
+};
 
 }
+  
+
