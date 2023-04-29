b7fe3e140e28228a4b32e02cace4e178af3e0514
Ike Okafor <okaforim@miamioh.edu>
2023-04-29 14:35:25 -0400

    o

--- src/app/requests/request-detail/request-detail.component.ts
+++ src/app/requests/request-detail/request-detail.component.ts
@@ -7,5 +7,29 @@ import { Component } from '@angular/core';
 })
 export class RequestDetailComponent {
 pageTitle: string = "Request Detail";
+request: any =
+
+{
+  "id": 1,
+  "description": "test request",
+  "justification": "test",
+  "rejectionReason": "test",
+  "deliveryMode": "test",
+  "submittedDate": "2023-03-08T00:00:00",
+  "dateNeeded": "2023-01-18",
+  "status": "Approved",
+  "total": 3629.7,
+  "user": {
+      "id": 1,
+      "userName": "username",
+      "firstName": "firstName",
+      "lastName": "lastName",
+      "phone": "5135551234",
+      "email": "admin@email.com",
+      "isReviewer": true,
+      "isAdmin": true
+  }
+}
 
 }
+
